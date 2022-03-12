39f2c4784105074a60ffeac65b73dba6374dc802
soberanoJr <tonysoberano@gmail.com>
2022-03-12 17:48:29 -0300

    user update

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -3,20 +3,58 @@ import type { MessagePatterns } from "../../types/MessagePatterns";
 import { jwtTokenValidation } from "../../middlewares/jwtTokenValidation";
 import { mongoDbConnect } from "../../middlewares/mongoDbConnect";
 import { UserModels } from "../../models/UserModels";
+import nc from 'next-connect';
+import {upload, cosmicImageUpload} from '../../services/cosmicImageUpload'
 
+const handler = nc()
+    .use(upload.single('file'))
+    .put(async (req: any, res: NextApiResponse<MessagePatterns>) => {
+        try {
+            const {userId} = req?.query;
+            const user = await UserModels.findById(userId);
+            
+            if(!user) {
+                return res.status(400).json({ error: `User ${user} not found.` });    
+            }
 
-const userEndpoint = async (req: NextApiRequest, res: NextApiResponse<MessagePatterns>) => {
-    try {
-        // Get user id
-        const { userId } = req?.query;
-        const user = await UserModels.findById(userId);
-        user.password = null;
+            const { name } = req.body;
+            if(name && name.length > 2) {
+                user.name = name;
+            }
 
-        return res.status(200).json(user);
-    } catch(e) {
-        console.log(e);
-        return res.status(400).json({ error: `User data not available for now (${e})` });
-    };
-};
+            const { file } = req;
+            if(file && file.originalname) {
+                // Send Multer image to Cosmic
+                const image = await cosmicImageUpload(req);
+                if(image && image.media && image.media.url) {
+                    user.avatar = image.media.url;
+                }
+            }
 
-export default jwtTokenValidation(mongoDbConnect(userEndpoint));
+            // Change data at db
+            await UserModels.findByIdAndUpdate({ _id : user._id }, user);
+
+            return res.status(200).json({ message: `User '${user.name}' successfully updated.` });
+
+        } catch(e) {
+            console.log(e);
+            return res.status(400).json({ error: `${e}` });
+        }
+    })
+    .get(async (req: NextApiRequest, res: NextApiResponse<MessagePatterns>) => {
+        try {
+            // Get user id
+            const { userId } = req?.query;
+            const user = await UserModels.findById(userId);
+            user.password = null;
+    
+            return res.status(200).json(user);
+        } catch(e) {
+            console.log(e);
+            return res.status(400).json({ error: `User data not available for now. (${e})` });
+        };
+    });
+
+export const config = { api: { bodyParser: false }};
+
+export default jwtTokenValidation(mongoDbConnect(handler));
